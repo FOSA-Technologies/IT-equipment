b18b89792f77b72ee926588250681fe624cfbf45
lubain <fadhelubain@gmail.com>
2026-09-23 13:27:18 +0300

    feat: add variable font imports for Inter and Orbitron

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "@/App";
+import "@fontsource-variable/inter";
+import "@fontsource-variable/orbitron";
 import "@/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
